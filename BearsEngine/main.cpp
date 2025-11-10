3802a9f81c706543eb9f4b62bbce60b73df02d76
Zhengyang Lyu <colt.github@outlook.com>
2025-11-10 23:21:16 +0100

    Imgui integration and compiling

--- BearsEngine/main.cpp
+++ BearsEngine/main.cpp
@@ -9,6 +9,11 @@
 #include "Shader.h"
 #include "Mesh.h"
 #include "Camera.h"
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 // Screen ettings
 const unsigned int SCR_WIDTH = 1200;
@@ -31,6 +36,17 @@ int main(int argc, char* argv[])
 	}
 
 	glfwMakeContextCurrent(m_mainWindow);
+	glfwSetFramebufferSizeCallback(m_mainWindow, framebuffer_size_callback);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(m_mainWindow, true);
+	ImGui_ImplOpenGL3_Init();
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -62,6 +78,10 @@ int main(int argc, char* argv[])
 
 	while (!glfwWindowShouldClose(m_mainWindow))
 	{
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -89,11 +109,28 @@ int main(int argc, char* argv[])
 		// Rendering
 		firstSphere.Render(lightingShader);
 
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(m_mainWindow);
 
 		glfwPollEvents();
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwTerminate();
+
 	return 0;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
